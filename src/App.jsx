1e9db09471d30a1b6d1f03d4c6e9f9b8ed1b762a
sherzod-raxmatullayev <shraxmatullayev6888@gmail.com>
2026-04-10 10:36:13 +0500

    1.3.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { routes } from "./utils/routes.jsx";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
+  const location = useLocation()
+  
+  // Header va Footer yashiriladigan sahifalar
+  const hideLayout = location.pathname.startsWith('/anime/')
+
   return (
     <>
-      <Header />
+      {!hideLayout && <Header />}
 
       <Routes>
         {routes.map((route) => (
